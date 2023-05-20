d404b8fd9cb5c5aeadb68349bb6fba1cf6e0ed0c
Jack Howorth <Jack.howorth27@gmail.com>
2023-05-20 14:38:04 +0100

    UI updates and buttons added (without functionality)

--- datahub/clientinstallations/views.py
+++ datahub/clientinstallations/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Client, ApplicationInstance, ServerGroup, Server
 from django.views import View
 
@@ -55,3 +55,22 @@ def servers(request):
 def servergroups(request):
     groups = ServerGroup.objects.all()
     return render(request, 'servergroups.html', {'groups': groups})
+
+def update_app(request, app_id):
+    app = get_object_or_404(ApplicationInstance, id=app_id)
+
+    if request.method == 'POST':
+        # Process the form data and update the application object
+        app.product = request.POST.get('product')
+        app.environment = request.POST.get('environment')
+        app.version = request.POST.get('version')
+        app.url = request.POST.get('url')
+        app.relationshipmanager = request.POST.get('relationshipmanager')
+        app.live = bool(request.POST.get('live'))
+        app.save()
+
+        # Redirect to a success page or perform any other desired actions
+        return redirect('app', pk=app.id)
+
+    context = {'app': app}
+    return render(request, 'update_app.html', context)
